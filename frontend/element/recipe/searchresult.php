1f91287ae222c884790e74aa25e9579dce786ee7
Jan Nox <jan.nox+github@pm.me>
2022-11-24 13:04:38 +0100

    "beautify" front-end.
    I'm more a back-end and abstraction guy. But okay =)

--- frontend/element/recipe/searchresult.php
+++ frontend/element/recipe/searchresult.php
@@ -13,16 +13,15 @@ $t = Translator::getInstance();
 
 $recipeInfo     = FrontendHelper::parseFile(Path::getInheritedPath('frontend/element/recipeinfo.php'), $data);
 echo <<<HTML
-<div class="col-md-12">
-    <div class="row">
-        <div class="col-md-3">
-            <a href="$url"><img src="https://placekitten.com/g/300/300"></a>
-        </div>
-        <div class="col-md-3">
-            <h3><a href="$url">{$t->translate($data->name)}</a></h3>
-            <hr />
-            $recipeInfo
-        </div>
+<hr />
+<div class="row">
+    <div class="col-md-4">
+        <a href="$url"><img src="https://placekitten.com/g/300/300"></a>
+    </div>
+    <div class="col-md-7">
+        <h3><a href="$url">{$t->translate($data->name)}</a></h3>
+        <hr />
+        $recipeInfo
     </div>
 </div>
 HTML;
