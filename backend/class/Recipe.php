d8c74db4842122312db2df0f4b605f5d76ef09cb
Jan Nox <jan.nox+github@pm.me>
2022-11-24 13:04:08 +0100

    A Recipe contains a creation DateTime

--- backend/class/Recipe.php
+++ backend/class/Recipe.php
@@ -1,6 +1,8 @@
 <?php declare(strict_types = 1);
 namespace noxkiwi\cookbook;
 
+use DateTime;
+
 /**
  * I am the Recipe Object.
  *
@@ -13,14 +15,15 @@ namespace noxkiwi\cookbook;
  */
 final class Recipe
 {
-    public int    $recipeId;
-    public string $name;
-    public string $description;
-    public float  $multiplier;
-    public float  $rating;
-    public int    $duration;
-    public int    $views;
-    public int    $skill;
+    public int      $recipeId;
+    public string   $name;
+    public string   $description;
+    public float    $multiplier;
+    public float    $rating;
+    public int      $duration;
+    public int      $views;
+    public int      $skill;
+    public DateTime $created;
     /** @var \noxkiwi\cookbook\Ingredient[] */
     public array $ingredients;
     /** @var \noxkiwi\cookbook\Rating[] */
